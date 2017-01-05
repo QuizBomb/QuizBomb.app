5af6e5064c46e2de716fc2aa62581fd88e3da6b0
Ioannis Mousmoutis <imousmoutis@gmail.com>
2017-01-05 09:57:49 +0200

    Simplified IndexController

--- src/main/java/gr/quizbomb/controller/IndexController.java
+++ src/main/java/gr/quizbomb/controller/IndexController.java
@@ -1,23 +1,16 @@
 package gr.quizbomb.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import gr.quizbomb.service.DummyService;
-
 @Controller
 public class IndexController {
-	
-	@Autowired
-	private DummyService dummyService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getIndex(Model model) {
 		
-		model.addAttribute("dummy", dummyService.findById(0));
 		return "index";
 	}
 
