2c92afd821b710a4726a47c199088c44928d70cd
Anastasios Leivaditis <you@example.com>
2017-01-14 23:07:18 +0200

    Added user services

--- src/main/java/gr/quizbomb/service/impl/UserServiceImpl.java
+++ src/main/java/gr/quizbomb/service/impl/UserServiceImpl.java
@@ -1,14 +1,26 @@
 package gr.quizbomb.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import gr.quizbomb.model.User;
+import gr.quizbomb.repository.UserDAO;
 import gr.quizbomb.service.UserService;
 
 @Service
 @Transactional
 public class UserServiceImpl extends GenericServiceImpl<User> implements UserService{
+	
+	@Autowired
+	private UserDAO dao;
+
+	@Override
+	public User findUserByUsernameAndPassword(String username, String password) {
+		
+	
+		return dao.findByUsernameAndPassword(username, password);
+	}
 	
 
 }
