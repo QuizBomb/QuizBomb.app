a7272d6aaa3d7ac949357419a5d6fa9c6382e41f
Elena Antonaki <elenanto101@gmail.com>
2017-01-18 14:03:13 +0200

    Finished createQuiz functionality

--- src/main/java/gr/quizbomb/service/QuizService.java
+++ src/main/java/gr/quizbomb/service/QuizService.java
@@ -1,12 +1,9 @@
 package gr.quizbomb.service;
 
-import java.util.List;
 import gr.quizbomb.model.Quiz;
 import gr.quizbomb.model.helper.CreateQuizSimpler;
 
 public interface QuizService extends GenericService<Quiz>{
-	
-	public void createNewQuiz(Quiz quiz, Long sClassId, Long courseId);
 
 	public void createNewQuiz(CreateQuizSimpler quiz, Long sClassId, Long courseId);
 }
