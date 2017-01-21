3ba57164da2cda0bd792c4c5ce057f7ba48bd0aa
Elena Antonaki <elenanto101@gmail.com>
2017-01-22 00:00:52 +0200

    Updated Student model

--- src/main/java/gr/quizbomb/model/Student.java
+++ src/main/java/gr/quizbomb/model/Student.java
@@ -1,9 +1,16 @@
 package gr.quizbomb.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 @Entity
 @Table(name = "student")
@@ -12,6 +19,10 @@ public class Student extends User {
 	@ManyToOne(optional = false)
     @JoinColumn(name="sclass_id")
 	private SClass sClass;
+	
+	@OneToMany(mappedBy="student")
+	@LazyCollection(LazyCollectionOption.FALSE)
+	private List<Score> score;
 	
 	public String getRole(){
 		
@@ -24,6 +35,14 @@ public class Student extends User {
 
 	public void setsClass(SClass sClass) {
 		this.sClass = sClass;
+	}
+
+	public List<Score> getScore() {
+		return score;
+	}
+
+	public void setScore(List<Score> score) {
+		this.score = score;
 	}
 
 }
