eda91c4a78b83486855b55b6be79eae941415795
Anastasios Leivaditis <galinhos3@gmail.com>
2017-01-24 03:00:06 +0200

    All DB scripts updated and unified to one

--- src/main/resources/scripts/FinalScript-DBinitialization.sql
+++ src/main/resources/scripts/FinalScript-DBinitialization.sql
@@ -1,3 +1,259 @@
+CREATE DATABASE  IF NOT EXISTS `quizbomb` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+USE `quizbomb`;
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fullName` varchar(45),
+  `username` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+--
+-- Table structure for table `professor`
+--
+
+CREATE TABLE IF NOT EXISTS `professor` (
+  `id` bigint(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `controller`
+--
+
+CREATE TABLE IF NOT EXISTS `controller` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `sclass`
+--
+
+CREATE TABLE IF NOT EXISTS `sclass` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `value` varchar(45) NOT NULL,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE IF NOT EXISTS `student` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sclass_id` bigint(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_student_sclass_id` FOREIGN KEY (`sclass_id`) REFERENCES `sclass` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE IF NOT EXISTS `course` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `psc` (Professor-SClass-Course)
+--
+
+CREATE TABLE IF NOT EXISTS `psc` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `professor_id` bigint(10) unsigned NOT NULL,
+  `sclass_id` bigint(10) unsigned NOT NULL,
+  `course_id` bigint(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_psc_sclass_id` FOREIGN KEY (`sclass_id`) REFERENCES `sclass` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_psc_course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_psc_professor_id` FOREIGN KEY (`professor_id`) REFERENCES `professor` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE IF NOT EXISTS `question` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `isapproved` tinyint(1) DEFAULT 0 NOT NULL,
+  `course_id` bigint(10) unsigned NOT NULL,
+  `sclass_id` bigint(10) unsigned NOT NULL,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_question_course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_question_sclass_id` FOREIGN KEY (`sclass_id`) REFERENCES `sclass` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+--
+-- Table structure for table `answer`
+--
+
+CREATE TABLE IF NOT EXISTS `answer` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `correct` tinyint(1) NOT NULL,
+  `question_id` bigint(10) unsigned NOT NULL,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_answer_question_id` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE IF NOT EXISTS `quiz` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `course_id` bigint(10) unsigned NOT NULL,
+  `sclass_id` bigint(10) unsigned NOT NULL,
+  `title` VARCHAR(150) NOT NULL,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_quiz_course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_quiz_sclass_id` FOREIGN KEY (`sclass_id`) REFERENCES `sclass` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `quiz_question`
+--
+
+CREATE TABLE IF NOT EXISTS `quiz_question` (
+  `quiz_id` bigint(10) unsigned NOT NULL,
+  `question_id` bigint(10) unsigned NOT NULL,
+  PRIMARY KEY (`quiz_id`,`question_id`),
+  CONSTRAINT `fk_quiz_question_question_id` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_quiz_question_quiz_id` FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `score`
+--
+
+CREATE TABLE IF NOT EXISTS `score` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `quiz_id` bigint(10) unsigned NOT NULL,
+  `student_id` bigint(10) unsigned NOT NULL,
+  `successrate` float NOT NULL,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_score_quiz_id` FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_score_student_id` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Eleytherios Mavrothalassitis', 'lefmav', 'lefmav@proodos.gr', 'lefmav1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Efthimis Tsolas', 'tsolas', 'tsolas@proodos.gr', 'tsolas1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Allahu Akbaridis', 'akbar', 'akbar@proodos.gr', 'boom1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Maria Zizi', 'zizi', 'zizi@proodos.gr', 'zizi1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Pier Patrick Nikolaou', 'pierp', 'pierp@proodos.gr', 'pierp1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Tasoulis Livas', 'livas', 'livas@proodos.gr', 'livas1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Giasemakis Giasemis', 'geiasou', 'geiasou@proodos.gr', 'makis1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Maria Arni', 'arnaki', 'arnaki@proodos.gr', 'arni1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Ioannis Kolyvas', 'jkol', 'jkol@proodos.gr', 'jkol86');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Kostas Tsekmes', 'tsakmaki', 'tsakmaki@proodos.gr', 'vromtsek1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Andronikos Arabatzis', 'aravas', 'aravas@proodos.gr', 'aravas1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Dimitrios Darras', 'ddaras', 'ddaras@proodos.gr', 'ddaras1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Alexandra Paraskelidou', 'alexpa', 'alexpa@proodos.gr', 'alexpa1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Kostas Pelonis', 'peloni', 'peloni@proodos.gr', 'peloni1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Ioanna Tsinikou', 'tsinikou', 'tsinikou@proodos.gr', 'boom1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Dimitris Stathis', 'dstathis', 'dstathis@proodos.gr', 'dstathis1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Marianthi Papanota', 'mpapa', 'mpapa@proodos.gr', 'mpapa1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Efthimis Larentzakis', 'larry', 'larry@proodos.gr', 'larry1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Marios Matalon', 'matalon', 'matalon@proodos.gr', 'matalon1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Nikodimos Aristoklis', 'nikari', 'nikari@proodos.gr', 'nikari1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Avgerinos Iasonidou', 'avge', 'avge@proodos.gr', 'avge1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Maria Perrone', 'perrone', 'perrone@proodos.gr', 'perrone1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Asimina Rapti', 'rapti', 'rapti@proodos.gr', 'rapti1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Mimis Androulakis', 'mimis', 'mimis@proodos.gr', 'mimis1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Nikos Alefantos', 'alefas', 'alefas@proodos.gr', 'alefas1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Maria Kalpaki', 'kalpa', 'kalpa@proodos.gr', 'kalpa1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Sakis Rouvas', 'sakiii', 'sakiii@proodos.gr', 'sakiii1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Dimitris Kosmas', 'kosmas', 'kosmas@proodos.gr', 'kosmas1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Nikos Kasimatis', 'kasi', 'kasi@proodos.gr', 'kasi1');
+INSERT INTO `quizbomb`.`user` (`fullName`, `username`, `email`, `password`) VALUES ('Giannis Karamouzis', 'jmouz', 'jmouz@proodos.gr', 'jmouz1');
+
+
+
+INSERT INTO `quizbomb`.`professor` (`id`) VALUES ('1');
+INSERT INTO `quizbomb`.`professor` (`id`) VALUES ('2');
+INSERT INTO `quizbomb`.`professor` (`id`) VALUES ('10');
+INSERT INTO `quizbomb`.`professor` (`id`) VALUES ('3');
+INSERT INTO `quizbomb`.`professor` (`id`) VALUES ('6');
+
+
+INSERT INTO `quizbomb`.`sclass` (`value`) VALUES ('A');
+INSERT INTO `quizbomb`.`sclass` (`value`) VALUES ('B');
+INSERT INTO `quizbomb`.`sclass` (`value`) VALUES ('C');
+
+INSERT INTO `quizbomb`.`controller` (`id`) VALUES ('9');
+
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('11', '1');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('13', '3');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('14', '2');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('15', '2');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('16', '1');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('17', '3');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('18', '3');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('19', '3');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('20', '1');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('21', '2');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('22', '1');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('23', '2');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('24', '2');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('25', '3');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('26', '1');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('27', '2');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('28', '3');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('29', '3');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('30', '1');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('4', '1');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('5', '2');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('7', '3');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('8', '1');
+INSERT INTO `quizbomb`.`student` (`id`, `sclass_id`) VALUES ('12', '3');
+
+
+INSERT INTO `quizbomb`.`course` (`name`) VALUES (N'Μαθηματικά');
+INSERT INTO `quizbomb`.`course` (`name`) VALUES (N'Φυσική');
+INSERT INTO `quizbomb`.`course` (`name`) VALUES (N'Χημεία');
+INSERT INTO `quizbomb`.`course` (`name`) VALUES (N'Ιστορία');
+INSERT INTO `quizbomb`.`course` (`name`) VALUES (N'Πληροφορική');
+INSERT INTO `quizbomb`.`course` (`name`) VALUES (N'Βιολογία');
+
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('1', '1', '1');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('1', '2', '1');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('1', '3', '1');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('2', '1', '2');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('2', '2', '2');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('2', '3', '2');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('3', '1', '3');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('3', '2', '3');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('3', '3', '3');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('10', '1', '4');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('10', '2', '4');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('10', '3', '4');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('6', '1', '5');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('6', '2', '5');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('6', '3', '5');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('10', '1', '6');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('10', '2', '6');
+INSERT INTO `quizbomb`.`psc` (`professor_id`, `sclass_id`, `course_id`) VALUES ('10', '3', '6');
+
+
 
 INSERT INTO `quizbomb`.`question` (`content`, `isapproved`, `course_id`, `sclass_id`) VALUES (N'Είμαστε ακίνητοι και κρατάμε με το χέρι μας μια μπάλα ποδοσφαίρου. Η μπάλα έχει βάρος 8 Ν και βρίσκεται 1,5 m απο το έδαφος. Το έργο της δύναμης του χεριού μας για χρονικό διάστημα 10 s είναι:', '1', '2', '1');
 INSERT INTO `quizbomb`.`question` (`content`, `isapproved`, `course_id`, `sclass_id`) VALUES (N'Μεταφέρουμε ένα σώμα απο το σημείο Α στο σημείο Β. Το έργο του βάρους του σώματος:', '1', '2', '1');
@@ -756,8 +1012,7 @@ INSERT INTO `quizbomb`.`answer` (`content`, `correct`, `question_id`) VALUES (N'
 INSERT INTO `quizbomb`.`answer` (`content`, `correct`, `question_id`) VALUES (N'Να επιδιωχθεί με πείσμα η νίκη.', '0', '138');
 INSERT INTO `quizbomb`.`answer` (`content`, `correct`, `question_id`) VALUES (N'Το α και το γ.', '1', '138');
 
-ALTER TABLE `quizbomb`.`quiz` 
-CHANGE COLUMN `title` `title` VARCHAR(150) NOT NULL ;
+
 
 
 INSERT INTO `quizbomb`.`quiz` (`course_id`, `sclass_id`, `title`) VALUES ('2', '2', N'Φυσική - Τεστ ερωτήσεων πολλαπλής επιλογής -  Β΄ Γυμνασίου');
